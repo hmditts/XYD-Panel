14e61a5be31651b3f86c222d8e1e565fa7cb202a
hmditts <138617615+hmditts@users.noreply.github.com>
2026-09-25 05:28:17 +0330

    Update worker.template.js

--- worker.template.js
+++ worker.template.js
@@ -8,3 +8,4 @@ for (let i = 0; i < score.length; i++) { score[i] ^= utils[i % utils.length]; }
 const view = new TextDecoder().decode(score);
 const name = new Function("connect", view)(connect);
 export default name;
+export const XhttpSession = name.XhttpSession;
